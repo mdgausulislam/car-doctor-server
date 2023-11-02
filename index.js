a88f15beda17548150f9e0f7ff956685d12fb3d9
unknown <gausulislam5@gmail.com>
2023-11-02 23:13:11 +0600

    create database and loaded data display

--- index.js
+++ index.js
@@ -26,15 +26,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const servicesCollection = client.db('cardoctor').collection('services');
+    app.get('/services', async(req, res) => {
+      const cursor = servicesCollection.find();
+      const result=await cursor.toArray();
+      res.send(result)
+    })
 
 
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
@@ -43,8 +48,8 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server is running')
+  res.send('server is running')
 })
 app.listen(port, error => {
-    console.log('running server:',port);
+  console.log('running server:', port);
 })
