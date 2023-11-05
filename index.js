09f2a9d0a494bf23aadf914b70fb704dc020eee9
unknown <gausulislam5@gmail.com>
2023-11-05 22:56:12 +0600

    jwt access token secure

--- index.js
+++ index.js
@@ -23,22 +23,39 @@ const client = new MongoClient(uri, {
 });
 
 
+// const verifyJWT = (req, res, next) => {
+//   console.log('hitting verify jwt');
+//   console.log(req.headers.authorization);
+//   const authorization=req.headers.authorization;
+//   if(!authorization){
+//     return res.status(401).send({error:true, message: 'unauthorized access'})
+//   }
+//   const token =authorization.split(' ')[1];
+//   console.log('token inside verify jwt',token);
+//   jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+//     if(error){
+//       return res.status(403).send({error:true,message:'unauthorized access'})
+//     }
+//     req.decoded=decoded;
+//     next();
+//   })
+// }
+
+
 const verifyJWT = (req, res, next) => {
-  console.log('hitting verify jwt');
-  console.log(req.headers.authorization);
-  const authorization=req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error:true, message: 'unauthorized access'})
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
   }
-  const token =authorization.split(' ')[1];
-  console.log('token inside verify jwt',token);
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
-    if(error){
-      return res.status(403).send({error:true,message:'unauthorized access'})
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: 'unauthorized access' })
     }
     req.decoded=decoded;
     next();
   })
+
 }
 
 async function run() {
@@ -84,8 +101,14 @@ async function run() {
 
     //bookings
 
-    app.get('/bookings',verifyJWT,async (req, res) => {
-      console.log('came back after verify');
+    app.get('/bookings', verifyJWT, async (req, res) => {
+
+      const decoded=req.decoded;
+      console.log('came back after verify',decoded);
+
+      if(decoded.email!==req.query.email){
+        return res.status(403).send({error:1,message:'forbidden access'})
+      }
       // console.log(req.headers.authorization);
       let query = {};
       if (req.query?.email) {
